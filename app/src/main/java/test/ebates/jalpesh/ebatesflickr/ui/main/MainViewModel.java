8756492ae0850ff4061feccd5353338ecf3d5891
jalpesh@uelifesciences.com <uels2009>
2018-11-29 14:54:58 +0530

    Base commit

---
+++ app/src/main/java/test/ebates/jalpesh/ebatesflickr/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package test.ebates.jalpesh.ebatesflickr.ui.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
